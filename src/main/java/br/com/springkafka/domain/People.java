0906fa8ddb8b47664a3a763a5fb6b3ed4447d971
Felipe Salles <prof.felipe.salles@gmail.com>
2022-04-15 01:42:25 -0300

    Cria entidades People e Book

---
+++ src/main/java/br/com/springkafka/domain/People.java
@@ -0,0 +1,24 @@
+package br.com.springkafka.domain;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class People {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String name;
+    private String cpf;
+
+    @OneToMany(mappedBy = "people")
+    private List<Book> books;
+}
